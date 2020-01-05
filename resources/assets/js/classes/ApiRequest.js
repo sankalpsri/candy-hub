8e5825199ea94e68625d6719f9c1b59ef49f303a
Sankalp Srivastava <sankalp@plobal.com>
2020-01-05 19:45:41 +0530

    Added Token in

--- resources/assets/js/classes/ApiRequest.js
+++ resources/assets/js/classes/ApiRequest.js
@@ -61,6 +61,7 @@ class ApiRequest {
                 data: data,
                 params: params,
                 headers: {
+                    'Authorization': 'Bearer ' + ApiRequest.readCookie("access_token"),
                     'Accept': 'application/json',
                     ...headers
                 }
@@ -75,6 +76,17 @@ class ApiRequest {
             });
         });
 
+    }
+
+    readCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
     }
 
     loadProducts(params, flatten = false) {
--- src/Http/Controllers/DashboardController.php
+++ src/Http/Controllers/DashboardController.php
@@ -3,11 +3,20 @@
 namespace GetCandy\Hub\Http\Controllers;
 
 use Carbon\Carbon;
+use Illuminate\Cookie\CookieJar;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Http\Response;
 
 class DashboardController extends Controller
 {
     public function getIndex()
     {
-        return view('hub::dashboard');
+        if (!isset($minutes)) {
+            $minutes = 10;
+        }
+        $response = new Response(view('hub::dashboard'));
+        $access = auth()->user()->createToken(env("APP_NAME"))->accessToken;
+        $response->withCookie(cookie('access_code', $access, $minutes));
+        return $response;
     }
 }
